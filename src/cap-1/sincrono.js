f1c6c9155c1199ff1accacb74298da0222bf1ad0
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-09-11 02:53:57 -0300

    Desenvolvimento de exemplo de código síncrono.

---
+++ src/cap-1/sincrono.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * Arquivo: sincrono.js
+ * Autor: Glaucia Lemos
+ * Descrição: Exemplo execução síncrona
+ * Data: 10/09/2018
+ * 
+ */
+
+console.log('1');
+
+t();
+
+console.log('3');
+
+function t() {
+    console.log('2');
+}
