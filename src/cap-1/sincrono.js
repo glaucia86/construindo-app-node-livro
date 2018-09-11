1674bc5ea39c9a658fc60f98bb96a6b27c32cac4
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-09-11 03:15:29 -0300

    Finalização do exemplo.

--- src/cap-1/sincrono.js
+++ src/cap-1/sincrono.js
@@ -1,18 +1,18 @@
 /**
- * 
  * Arquivo: sincrono.js
  * Autor: Glaucia Lemos
  * Descrição: Exemplo execução síncrona
  * Data: 10/09/2018
- * 
  */
 
-console.log('1');
+console.log('1'); // saída 1
 
-t();
+t(); // saída 2
 
-console.log('3');
+console.log('3'); // saída 3
 
 function t() {
-    console.log('2');
+  console.log('2');
 }
+
+// Saída no console: 1,2,3
